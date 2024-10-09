3c59575d53a342ce22f9da3aca38af5b3c376b9b
Micheil <fouriemicheil1@gmail.com>
2024-10-10 00:31:51 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+
+// d3.select("body").append("h1").text("Learning D3");
+
+// d3.selectAll("li").text("list item")
+
+const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
